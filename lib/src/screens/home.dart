c038daac272c3cb97d188007cdb7fd0c12bbe9cb
Tyler Swindell <electromagnetic.jolt@gmail.com>
2019-02-28 14:00:30 -0500

    cat animation

---
+++ lib/src/screens/home.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import '../screens/widgets/cat.dart';
+
+class Home extends StatefulWidget {
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> with TickerProviderStateMixin {
+  Animation<double> catAnimation;
+  AnimationController catController;
+
+  initState() {
+    super.initState();
+
+    catController = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this,
+    );
+    catAnimation = Tween(begin: 0.0, end: 100.0)
+        .animate(CurvedAnimation(parent: catController, curve: Curves.easeIn));
+  }
+
+  Widget build(context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Animation!'),
+      ),
+      body: buildAnimation(),
+    );
+  }
+
+  Widget buildAnimation() {
+    return Cat();
+  }
+}
---
+++ lib/src/screens/widgets/cat.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Cat extends StatelessWidget{
+  build(context){
+    return Image.network('https://i.imgur.com/QwhZRyL.png'
+    );
+  }
+}
